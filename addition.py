1572d7ef528c0ee6dee9b11557d51b604799dcc9
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-03-03 11:51:10 +0530

    Delete addition.py

--- addition.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter the first number: "))
-b=int(input("enter the second number: "))
-
-print(a+b)
-
-c=int(input("enter the first number: "))
-d=int(input("enter the second number: "))
-
-print(c*d)

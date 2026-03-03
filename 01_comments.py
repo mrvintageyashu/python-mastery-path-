a3e2fa555b4b985375c8acf003b281ff5bef97e5
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-03-03 11:50:41 +0530

    Delete 01_comments.py

--- 01_comments.py
+++
@@ -1,7 +0,0 @@
-print("ok so i am just practicing comments here")
-#so this is a comment actully if you see
-#anyone seeing this code file now i task is write calc program here of python 
-#so if my comment reached to you my work alredy done
-'''nfasnfanfknasfknasknjfaksnflaknflanflanf
-nasfknasknfkasnfa
-lansfksanfaskn;f'''

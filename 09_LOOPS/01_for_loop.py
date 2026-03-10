d033477c76b45242c11d55dbff13cea45bc2f697
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-03-10 21:56:36 +0530

    Add files via upload

---
+++ 09_LOOPS/01_for_loop.py
@@ -0,0 +1,6 @@
+for i in range(1,26):
+    print(i)
+#in range number will print always less than 1 if its 26 it will be 25 like that 
+
+for i in range(1,11):
+    print("5 x",i,"=",5*(i))
---
+++ 09_LOOPS/06_pass.py
@@ -0,0 +1,4 @@
+i=3
+if i==12:
+    pass #do nothing means pass in python
+print("end of program")

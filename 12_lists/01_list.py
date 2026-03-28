8aebbd3517379549ac36e92d6da8f33dbd8f5fd5
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-03-28 14:31:22 +0530

    Add files via upload

---
+++ 12_lists/01_list.py
@@ -0,0 +1,5 @@
+marks = [18,14,20,18]
+mixed = ["yash","ohio",5.2,67]
+print(marks,mixed)
+print(marks[0])
+print(mixed[1:3])
---
+++ 12_lists/02_list_methods.py
@@ -0,0 +1,7 @@
+marks=[33,22,333,222]
+extra_marks=[29824,3434,2323]
+marks.append(67)
+print(marks)
+marks.extend(extra_marks)
+print(marks)
+print(marks.count(33))

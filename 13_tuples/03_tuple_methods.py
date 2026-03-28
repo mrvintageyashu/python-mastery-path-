4dd57efb35aa68882f992d94f0a933222ed09384
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-03-28 14:33:18 +0530

    Add files via upload

---
+++ 13_tuples/03_tuple_methods.py
@@ -0,0 +1,3 @@
+t=(44,333,423,22)
+print(t.count(44))
+print(t.index(423)) 
---
+++ 13_tuples/0_tuple.py
@@ -0,0 +1,5 @@
+my_tuple=(67,69,25,22)
+print(my_tuple)
+print(my_tuple[2])
+#my_tuple[0]=22 we cant modify tuple its illegal to do mannn 
+single_element=(4,)#when we declare single element tuple  we need "," always either it will show error

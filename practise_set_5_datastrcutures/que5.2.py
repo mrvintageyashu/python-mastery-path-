0956d511c381fa69c213651d40ad2659f5b4c68e
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-04-01 21:29:09 +0530

    Add files via upload

---
+++ practise_set_5_datastrcutures/que5.2.py
@@ -0,0 +1,5 @@
+diary={"yash":9289839892,"danid":87398739773298,"sanju":388320833232,"neon":3820980239802983}
+print(diary.keys())
+print(diary.values())
+for key, value in diary.items():
+    print(key,value)

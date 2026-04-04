75b3297b71919083d298b7f8fe12b3624c61369b
Mr.Vintage Yash <chavhanyashanil@gmail.com>
2026-04-04 21:46:08 +0530

    Add files via upload

---
+++ 16_OOPS/03_instances_class.py
@@ -0,0 +1,16 @@
+class Employee:
+    company="yintage"
+    def __init__(self,salary,name,bond,company):
+        self.salary=salary
+        self.name=name
+        self.bond=bond
+        self.company=company
+
+    def get_salary(self):
+        pass
+    def get_info(self):
+        print(f"the name of employee is {self.name}. his salary is {self.salary}. the bond is for {self.bond}years")
+
+e1=Employee(3400,"ridhi",9,"google")
+print(e1.company)
+print(Employee.company)
